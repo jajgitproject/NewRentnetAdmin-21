e9399ea66072046608bc7e98aa3332dcd97df26d
shikhar <jajgitproject@gmail.com>
2026-06-16 12:36:49 +0530

    ss

--- src/app/printInvoiceSummary/printInvoiceSummary-routing.module.ts
+++ src/app/printInvoiceSummary/printInvoiceSummary-routing.module.ts
@@ -1,18 +1,35 @@
-// @ts-nocheck
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PrintInvoiceSummaryComponent } from './printInvoiceSummary.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: PrintInvoiceSummaryComponent
-  }
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class PrintInvoiceSummaryRoutingModule {}
-
+// @ts-nocheck
+
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { PrintInvoiceSummaryComponent } from './printInvoiceSummary.component';
+
+
+
+const routes: Routes = [
+
+  {
+
+    path: '',
+
+    component: PrintInvoiceSummaryComponent
+
+  }
+
+];
+
+
+
+@NgModule({
+
+  imports: [RouterModule.forChild(routes)],
+
+  exports: [RouterModule]
+
+})
+
+export class PrintInvoiceSummaryRoutingModule {}
+
+
